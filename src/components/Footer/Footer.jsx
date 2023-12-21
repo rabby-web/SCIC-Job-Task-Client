7a12fe69f55584bb99f37c704214445471c12771
rabby-web <rabby.webdeveloper@gmail.com>
2023-12-21 15:05:53 +0600

    tectvison footer add

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,37 @@
 const Footer = () => {
   return (
-    <div className="w-full h-auto justify-center bg-[url(https://img.freepik.com/free-vector/3d-earth-graphic-symbolizing-global-trade-illustration_456031-125.jpg?w=1380&t=st=1697551135~exp=1697551735~hmac=34ba0890409fe3465d9c7b1450ae9ac5f42478b77f32b255b49c66be2526b918)] bg-no-repeat bg-cover bg-center">
-      <footer className="footer footer-center p-10  text-white rounded bg-[#070607c0]">
-        <nav className="grid grid-flow-col gap-4">
-          <a className="link link-hover">Home</a>
+    <div className="bg-[#222935]  md:p-6 p-6">
+      <footer className="footer md:p-6 text-white max-w-7xl mx-auto">
+        <aside>
+          <img
+            src="https://www.technovisionbd.com/wp-content/themes/tet/assets/images/logo.png"
+            alt=""
+          />
+          <p>
+            <span className="text-xl">SCC Technovision Inc</span>
+            <br />
+            <address>Level-4, 34, Awal Centre, Banani, Dhaka</address>
+          </p>
+        </aside>
+        <nav className="">
+          <header className="footer-title text-white">Company</header>
+          <a className="/home">Home</a>
           <a className="link link-hover">About</a>
           <a className="link link-hover">Blog</a>
-          <a className="link link-hover">Login</a>
+          <a className="/login">Login</a>
           <a className="link link-hover">Register</a>
         </nav>
         <nav>
+          <header className="footer-title text-white">Company</header>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </nav>
+        <nav>
+          <header className="footer-title text-white">Social</header>
           <div className="grid grid-flow-col gap-4">
-            <a>
+            <a href="https://twitter.com/RabbyNaeem29188">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -22,7 +42,7 @@ const Footer = () => {
                 <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
               </svg>
             </a>
-            <a>
+            <a href="https://www.youtube.com/watch?v=Q52Zcq09Hrg">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -33,7 +53,7 @@ const Footer = () => {
                 <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
               </svg>
             </a>
-            <a>
+            <a href="https://www.facebook.com/ridoy.babu.3538/">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -46,10 +66,10 @@ const Footer = () => {
             </a>
           </div>
         </nav>
-        <aside>
-          <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
-        </aside>
       </footer>
+      <p className="text-center text-sm text-white pb-3">
+        Copyright © 2023 - All right reserved by ACME Industries Ltd
+      </p>
     </div>
   );
 };
