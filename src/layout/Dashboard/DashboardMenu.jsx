3f81f359580ab4ed81a2d8d0e22d9ecbedca863a
rabby-web <rabby.webdeveloper@gmail.com>
2023-12-22 16:34:37 +0600

    icon change

--- src/layout/Dashboard/DashboardMenu.jsx
+++ src/layout/Dashboard/DashboardMenu.jsx
@@ -1,5 +1,7 @@
 import { FaHome } from "react-icons/fa";
 import { MdBookmarkAdded } from "react-icons/md";
+import { FaExternalLinkSquareAlt } from "react-icons/fa";
+import { MdManageSearch } from "react-icons/md";
 
 import { Link } from "react-router-dom";
 
@@ -17,14 +19,14 @@ const DashboardMenu = () => {
         to={"/dashboard/allTask"}
         className="w-full my-2 py-3 px-5 text-dark-01 text-left text-lg font-medium bg-gray-100 hover:bg-gray-200 duration-500 flex items-center gap-3"
       >
-        <FaHome className="text-2xl" />{" "}
+        <FaExternalLinkSquareAlt className="text-2xl" />{" "}
         <span className="hidden md:block">AllTask</span>
       </Link>
       <Link
         to={"/dashboard/manageTask"}
         className="w-full my-2 py-3 px-5 text-dark-01 text-left text-lg font-medium bg-gray-100 hover:bg-gray-200 duration-500 flex items-center gap-3"
       >
-        <FaHome className="text-2xl" />{" "}
+        <MdManageSearch className="text-2xl" />{" "}
         <span className="hidden md:block">Manage Task</span>
       </Link>
       <Link
